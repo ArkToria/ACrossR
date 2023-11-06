f14ad7162d50d20c73e3915622cf46625e280c04
khonoka <kagiyamahonoka@protonmail.com>
2023-11-06 19:05:44 +0800

    feat: submodules

--- src/rust/build.rs
+++ src/rust/build.rs
@@ -1,6 +1,6 @@
 fn main() -> Result<(), Box<dyn std::error::Error>> {
-    println!("cargo:rerun-if-changed=proto/acolors.proto");
-    tonic_build::compile_protos("proto/acolors.proto")?;
+    println!("cargo:rerun-if-changed=../../submodules/AColoRS/proto/acolors.proto");
+    tonic_build::compile_protos("../../submodules/AColoRS/proto/acolors.proto")?;
     println!("cargo:rustc-link-lib=crypto");
     println!("cargo:rustc-link-lib=ssl");
 
