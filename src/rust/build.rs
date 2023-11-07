2defe5ef9bdc9d1089db405f908f2c20546db112
khonoka <kagiyamahonoka@protonmail.com>
2023-11-07 09:31:45 +0800

    build(cargo): remove useless build command

--- src/rust/build.rs
+++ src/rust/build.rs
@@ -1,8 +1,3 @@
 fn main() -> Result<(), Box<dyn std::error::Error>> {
-    println!("cargo:rerun-if-changed=../../submodules/AColoRS/proto/acolors.proto");
-    tonic_build::compile_protos("../../submodules/AColoRS/proto/acolors.proto")?;
-    println!("cargo:rustc-link-lib=crypto");
-    println!("cargo:rustc-link-lib=ssl");
-
     Ok(())
 }
