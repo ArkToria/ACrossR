4c99678f39fa36d35d20f833a8c2ccdf47dc6359
khonoka <kagiyamahonoka@protonmail.com>
2023-11-08 08:19:34 +0800

    refactor: clean anyhow Error

--- src/rust/src/ffi/profile.rs
+++ src/rust/src/ffi/profile.rs
@@ -75,17 +75,51 @@ pub mod ffi {
 
     }
 }
-async fn async_empty_group_by_id(channel: Endpoint, group_id: i64) -> anyhow::Result<()> {
+#[derive(Debug)]
+pub enum ProfileError {
+    Connect(tonic::transport::Error),
+    Request(tonic::Status),
+    NoEndpoint,
+}
+impl std::fmt::Display for ProfileError {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match self {
+            ProfileError::Connect(_) => f.write_str("failed to connect"),
+            ProfileError::Request(_) => f.write_str("failed to request"),
+            ProfileError::NoEndpoint => f.write_str("no endpoint"),
+        }
+    }
+}
+impl std::error::Error for ProfileError {
+    fn source(&self) -> Option<&(dyn std::error::Error + 'static)> {
+        match self {
+            ProfileError::Connect(e) => Some(e),
+            ProfileError::Request(e) => Some(e),
+            ProfileError::NoEndpoint => None,
+        }
+    }
+}
+impl From<tonic::transport::Error> for ProfileError {
+    fn from(err: tonic::transport::Error) -> Self {
+        Self::Connect(err)
+    }
+}
+impl From<tonic::Status> for ProfileError {
+    fn from(err: tonic::Status) -> Self {
+        Self::Request(err)
+    }
+}
+async fn async_empty_group_by_id(channel: Endpoint, group_id: i64) -> Result<(), ProfileError> {
     let mut client = ProfileManagerClient::connect(channel).await?;
     let request = tonic::Request::new(EmptyGroupByIdRequest { group_id });
     let _response = client.empty_group_by_id(request).await?;
     Ok(())
 }
-pub fn empty_group_by_id(group_id: i64) -> anyhow::Result<()> {
+pub fn empty_group_by_id(group_id: i64) -> Result<(), ProfileError> {
     let lock = ACROSS_RPC.lock().unwrap();
     let channel = match &lock.endpoint {
         Some(c) => c.clone(),
-        None => return Err(anyhow::anyhow!("No Endpoint")),
+        None => return Err(ProfileError::NoEndpoint),
     };
     let result = ACROSS_RUNTIME
         .lock()
@@ -97,7 +131,7 @@ async fn async_update_group_by_id(
     channel: Endpoint,
     group_id: i64,
     use_proxy: bool,
-) -> anyhow::Result<()> {
+) -> Result<(), ProfileError> {
     let mut client = ProfileManagerClient::connect(channel).await?;
     let request = tonic::Request::new(UpdateGroupByIdRequest {
         group_id,
@@ -106,11 +140,11 @@ async fn async_update_group_by_id(
     let _response = client.update_group_by_id(request).await?;
     Ok(())
 }
-pub fn update_group_by_id(group_id: i64, use_proxy: bool) -> anyhow::Result<()> {
+pub fn update_group_by_id(group_id: i64, use_proxy: bool) -> Result<(), ProfileError> {
     let lock = ACROSS_RPC.lock().unwrap();
     let channel = match &lock.endpoint {
         Some(c) => c.clone(),
-        None => return Err(anyhow::anyhow!("No Endpoint")),
+        None => return Err(ProfileError::NoEndpoint),
     };
     let result = ACROSS_RUNTIME
         .lock()
@@ -122,17 +156,17 @@ async fn async_append_node_by_url(
     channel: Endpoint,
     group_id: i64,
     url: String,
-) -> anyhow::Result<i64> {
+) -> Result<i64, ProfileError> {
     let mut client = ProfileManagerClient::connect(channel).await?;
     let request = tonic::Request::new(AppendNodeByUrlRequest { url, group_id });
     let response = client.append_node_by_url(request).await?;
     Ok(response.into_inner().node_id)
 }
-pub fn append_node_by_url(group_id: i64, url: String) -> anyhow::Result<i64> {
+pub fn append_node_by_url(group_id: i64, url: String) -> Result<i64, ProfileError> {
     let lock = ACROSS_RPC.lock().unwrap();
     let channel = match &lock.endpoint {
         Some(c) => c.clone(),
-        None => return Err(anyhow::anyhow!("No Endpoint")),
+        None => return Err(ProfileError::NoEndpoint),
     };
     let result = ACROSS_RUNTIME
         .lock()
@@ -144,7 +178,7 @@ async fn async_append_node(
     channel: Endpoint,
     group_id: i64,
     node_data: NodeData,
-) -> anyhow::Result<i64> {
+) -> Result<i64, ProfileError> {
     let mut client = ProfileManagerClient::connect(channel).await?;
     let request = tonic::Request::new(AppendNodeRequest {
         data: Some(node_data),
@@ -153,11 +187,11 @@ async fn async_append_node(
     let response = client.append_node(request).await?;
     Ok(response.into_inner().node_id)
 }
-pub fn append_node(group_id: i64, node_data: ffi::NodeData) -> anyhow::Result<i64> {
+pub fn append_node(group_id: i64, node_data: ffi::NodeData) -> Result<i64, ProfileError> {
     let lock = ACROSS_RPC.lock().unwrap();
     let channel = match &lock.endpoint {
         Some(c) => c.clone(),
-        None => return Err(anyhow::anyhow!("No Endpoint")),
+        None => return Err(ProfileError::NoEndpoint),
     };
     let result = ACROSS_RUNTIME.lock().unwrap().block_on(async_append_node(
         channel,
@@ -166,7 +200,7 @@ pub fn append_node(group_id: i64, node_data: ffi::NodeData) -> anyhow::Result<i6
     ));
     result
 }
-async fn async_append_group(channel: Endpoint, group_data: GroupData) -> anyhow::Result<i64> {
+async fn async_append_group(channel: Endpoint, group_data: GroupData) -> Result<i64, ProfileError> {
     let mut client = ProfileManagerClient::connect(channel).await?;
     let request = tonic::Request::new(AppendGroupRequest {
         data: Some(group_data),
@@ -174,11 +208,11 @@ async fn async_append_group(channel: Endpoint, group_data: GroupData) -> anyhow:
     let response = client.append_group(request).await?;
     Ok(response.into_inner().group_id)
 }
-pub fn append_group(group_data: ffi::GroupData) -> anyhow::Result<i64> {
+pub fn append_group(group_data: ffi::GroupData) -> Result<i64, ProfileError> {
     let lock = ACROSS_RPC.lock().unwrap();
     let channel = match &lock.endpoint {
         Some(c) => c.clone(),
-        None => return Err(anyhow::anyhow!("No Endpoint")),
+        None => return Err(ProfileError::NoEndpoint),
     };
     let result = ACROSS_RUNTIME
         .lock()
@@ -186,17 +220,17 @@ pub fn append_group(group_data: ffi::GroupData) -> anyhow::Result<i64> {
         .block_on(async_append_group(channel, group_data.into()));
     result
 }
-async fn async_remove_group_by_id(channel: Endpoint, group_id: i64) -> anyhow::Result<()> {
+async fn async_remove_group_by_id(channel: Endpoint, group_id: i64) -> Result<(), ProfileError> {
     let mut client = ProfileManagerClient::connect(channel).await?;
     let request = tonic::Request::new(RemoveGroupByIdRequest { group_id });
     let _response = client.remove_group_by_id(request).await?;
     Ok(())
 }
-pub fn remove_group_by_id(group_id: i64) -> anyhow::Result<()> {
+pub fn remove_group_by_id(group_id: i64) -> Result<(), ProfileError> {
     let lock = ACROSS_RPC.lock().unwrap();
     let channel = match &lock.endpoint {
         Some(c) => c.clone(),
-        None => return Err(anyhow::anyhow!("No Endpoint")),
+        None => return Err(ProfileError::NoEndpoint),
     };
     let result = ACROSS_RUNTIME
         .lock()
@@ -204,17 +238,17 @@ pub fn remove_group_by_id(group_id: i64) -> anyhow::Result<()> {
         .block_on(async_remove_group_by_id(channel, group_id));
     result
 }
-async fn async_remove_node_by_id(channel: Endpoint, node_id: i64) -> anyhow::Result<()> {
+async fn async_remove_node_by_id(channel: Endpoint, node_id: i64) -> Result<(), ProfileError> {
     let mut client = ProfileManagerClient::connect(channel).await?;
     let request = tonic::Request::new(RemoveNodeByIdRequest { node_id });
     let _response = client.remove_node_by_id(request).await?;
     Ok(())
 }
-pub fn remove_node_by_id(node_id: i64) -> anyhow::Result<()> {
+pub fn remove_node_by_id(node_id: i64) -> Result<(), ProfileError> {
     let lock = ACROSS_RPC.lock().unwrap();
     let channel = match &lock.endpoint {
         Some(c) => c.clone(),
-        None => return Err(anyhow::anyhow!("No Endpoint")),
+        None => return Err(ProfileError::NoEndpoint),
     };
     let result = ACROSS_RUNTIME
         .lock()
@@ -222,17 +256,21 @@ pub fn remove_node_by_id(node_id: i64) -> anyhow::Result<()> {
         .block_on(async_remove_node_by_id(channel, node_id));
     result
 }
-async fn async_set_node_by_url(channel: Endpoint, node_id: i64, url: String) -> anyhow::Result<()> {
+async fn async_set_node_by_url(
+    channel: Endpoint,
+    node_id: i64,
+    url: String,
+) -> Result<(), ProfileError> {
     let mut client = ProfileManagerClient::connect(channel).await?;
     let request = tonic::Request::new(SetNodeByUrlRequest { node_id, url });
     let _response = client.set_node_by_url(request).await?;
     Ok(())
 }
-pub fn set_node_by_url(node_id: i64, url: String) -> anyhow::Result<()> {
+pub fn set_node_by_url(node_id: i64, url: String) -> Result<(), ProfileError> {
     let lock = ACROSS_RPC.lock().unwrap();
     let channel = match &lock.endpoint {
         Some(c) => c.clone(),
-        None => return Err(anyhow::anyhow!("No Endpoint")),
+        None => return Err(ProfileError::NoEndpoint),
     };
     let result = ACROSS_RUNTIME
         .lock()
@@ -244,7 +282,7 @@ async fn async_set_node_by_id(
     channel: Endpoint,
     node_id: i64,
     node_data: ffi::NodeData,
-) -> anyhow::Result<()> {
+) -> Result<(), ProfileError> {
     let mut client = ProfileManagerClient::connect(channel).await?;
     let request = tonic::Request::new(SetNodeByIdRequest {
         node_id,
@@ -253,11 +291,11 @@ async fn async_set_node_by_id(
     let _response = client.set_node_by_id(request).await?;
     Ok(())
 }
-pub fn set_node_by_id(node_id: i64, node_data: ffi::NodeData) -> anyhow::Result<()> {
+pub fn set_node_by_id(node_id: i64, node_data: ffi::NodeData) -> Result<(), ProfileError> {
     let lock = ACROSS_RPC.lock().unwrap();
     let channel = match &lock.endpoint {
         Some(c) => c.clone(),
-        None => return Err(anyhow::anyhow!("No Endpoint")),
+        None => return Err(ProfileError::NoEndpoint),
     };
     let result = ACROSS_RUNTIME
         .lock()
@@ -269,7 +307,7 @@ async fn async_set_group_by_id(
     channel: Endpoint,
     group_id: i64,
     group_data: ffi::GroupData,
-) -> anyhow::Result<()> {
+) -> Result<(), ProfileError> {
     let mut client = ProfileManagerClient::connect(channel).await?;
     let request = tonic::Request::new(SetGroupByIdRequest {
         group_id,
@@ -278,11 +316,11 @@ async fn async_set_group_by_id(
     let _response = client.set_group_by_id(request).await?;
     Ok(())
 }
-pub fn set_group_by_id(group_id: i64, group_data: ffi::GroupData) -> anyhow::Result<()> {
+pub fn set_group_by_id(group_id: i64, group_data: ffi::GroupData) -> Result<(), ProfileError> {
     let lock = ACROSS_RPC.lock().unwrap();
     let channel = match &lock.endpoint {
         Some(c) => c.clone(),
-        None => return Err(anyhow::anyhow!("No Endpoint")),
+        None => return Err(ProfileError::NoEndpoint),
     };
     let result = ACROSS_RUNTIME
         .lock()
@@ -290,17 +328,20 @@ pub fn set_group_by_id(group_id: i64, group_data: ffi::GroupData) -> anyhow::Res
         .block_on(async_set_group_by_id(channel, group_id, group_data));
     result
 }
-async fn async_get_node_by_id(channel: Endpoint, node_id: i64) -> anyhow::Result<ffi::NodeData> {
+async fn async_get_node_by_id(
+    channel: Endpoint,
+    node_id: i64,
+) -> Result<ffi::NodeData, ProfileError> {
     let mut client = ProfileManagerClient::connect(channel).await?;
     let request = tonic::Request::new(GetNodeByIdRequest { node_id });
     let response = client.get_node_by_id(request).await?;
     Ok(response.into_inner().into())
 }
-pub fn get_node_by_id(node_id: i64) -> anyhow::Result<ffi::NodeData> {
+pub fn get_node_by_id(node_id: i64) -> Result<ffi::NodeData, ProfileError> {
     let lock = ACROSS_RPC.lock().unwrap();
     let channel = match &lock.endpoint {
         Some(c) => c.clone(),
-        None => return Err(anyhow::anyhow!("No Endpoint")),
+        None => return Err(ProfileError::NoEndpoint),
     };
     let node_data = ACROSS_RUNTIME
         .lock()
@@ -308,17 +349,20 @@ pub fn get_node_by_id(node_id: i64) -> anyhow::Result<ffi::NodeData> {
         .block_on(async_get_node_by_id(channel, node_id));
     node_data
 }
-async fn async_get_group_by_id(channel: Endpoint, group_id: i64) -> anyhow::Result<ffi::GroupData> {
+async fn async_get_group_by_id(
+    channel: Endpoint,
+    group_id: i64,
+) -> Result<ffi::GroupData, ProfileError> {
     let mut client = ProfileManagerClient::connect(channel).await?;
     let request = tonic::Request::new(GetGroupByIdRequest { group_id });
     let response = client.get_group_by_id(request).await?;
     Ok(response.into_inner().into())
 }
-pub fn get_group_by_id(group_id: i64) -> anyhow::Result<ffi::GroupData> {
+pub fn get_group_by_id(group_id: i64) -> Result<ffi::GroupData, ProfileError> {
     let lock = ACROSS_RPC.lock().unwrap();
     let channel = match &lock.endpoint {
         Some(c) => c.clone(),
-        None => return Err(anyhow::anyhow!("No Endpoint")),
+        None => return Err(ProfileError::NoEndpoint),
     };
     let group_data = ACROSS_RUNTIME
         .lock()
@@ -326,17 +370,20 @@ pub fn get_group_by_id(group_id: i64) -> anyhow::Result<ffi::GroupData> {
         .block_on(async_get_group_by_id(channel, group_id));
     group_data
 }
-async fn async_list_all_nodes(channel: Endpoint, group_id: i64) -> anyhow::Result<ffi::NodeList> {
+async fn async_list_all_nodes(
+    channel: Endpoint,
+    group_id: i64,
+) -> Result<ffi::NodeList, ProfileError> {
     let mut client = ProfileManagerClient::connect(channel).await?;
     let request = tonic::Request::new(ListAllNodesRequest { group_id });
     let response = client.list_all_nodes(request).await?;
     Ok(response.into_inner().into())
 }
-pub fn list_all_nodes(group_id: i64) -> anyhow::Result<ffi::NodeList> {
+pub fn list_all_nodes(group_id: i64) -> Result<ffi::NodeList, ProfileError> {
     let lock = ACROSS_RPC.lock().unwrap();
     let channel = match &lock.endpoint {
         Some(c) => c.clone(),
-        None => return Err(anyhow::anyhow!("No Endpoint")),
+        None => return Err(ProfileError::NoEndpoint),
     };
     let node_list = ACROSS_RUNTIME
         .lock()
@@ -344,17 +391,17 @@ pub fn list_all_nodes(group_id: i64) -> anyhow::Result<ffi::NodeList> {
         .block_on(async_list_all_nodes(channel, group_id));
     node_list
 }
-async fn async_count_nodes(channel: Endpoint, group_id: i64) -> anyhow::Result<u64> {
+async fn async_count_nodes(channel: Endpoint, group_id: i64) -> Result<u64, ProfileError> {
     let mut client = ProfileManagerClient::connect(channel).await?;
     let request = tonic::Request::new(CountNodesRequest { group_id });
     let response = client.count_nodes(request).await?;
     Ok(response.into_inner().count)
 }
-pub fn count_nodes(group_id: i64) -> anyhow::Result<u64> {
+pub fn count_nodes(group_id: i64) -> Result<u64, ProfileError> {
     let lock = ACROSS_RPC.lock().unwrap();
     let channel = match &lock.endpoint {
         Some(c) => c.clone(),
-        None => return Err(anyhow::anyhow!("No Endpoint")),
+        None => return Err(ProfileError::NoEndpoint),
     };
     let count = ACROSS_RUNTIME
         .lock()
@@ -362,17 +409,17 @@ pub fn count_nodes(group_id: i64) -> anyhow::Result<u64> {
         .block_on(async_count_nodes(channel, group_id));
     count
 }
-async fn async_list_all_groups(channel: Endpoint) -> anyhow::Result<ffi::GroupList> {
+async fn async_list_all_groups(channel: Endpoint) -> Result<ffi::GroupList, ProfileError> {
     let mut client = ProfileManagerClient::connect(channel).await?;
     let request = tonic::Request::new(ListAllGroupsRequest {});
     let response = client.list_all_groups(request).await?;
     Ok(response.into_inner().into())
 }
-pub fn list_all_groups() -> anyhow::Result<ffi::GroupList> {
+pub fn list_all_groups() -> Result<ffi::GroupList, ProfileError> {
     let lock = ACROSS_RPC.lock().unwrap();
     let channel = match &lock.endpoint {
         Some(c) => c.clone(),
-        None => return Err(anyhow::anyhow!("No Endpoint")),
+        None => return Err(ProfileError::NoEndpoint),
     };
     let group_list = ACROSS_RUNTIME
         .lock()
@@ -381,17 +428,17 @@ pub fn list_all_groups() -> anyhow::Result<ffi::GroupList> {
     group_list
 }
 
-async fn async_count_groups(channel: Endpoint) -> anyhow::Result<u64> {
+async fn async_count_groups(channel: Endpoint) -> Result<u64, ProfileError> {
     let mut client = ProfileManagerClient::connect(channel).await?;
     let request = tonic::Request::new(CountGroupsRequest {});
     let response = client.count_groups(request).await?;
     Ok(response.into_inner().count)
 }
-pub fn count_groups() -> anyhow::Result<u64> {
+pub fn count_groups() -> Result<u64, ProfileError> {
     let lock = ACROSS_RPC.lock().unwrap();
     let channel = match &lock.endpoint {
         Some(c) => c.clone(),
-        None => return Err(anyhow::anyhow!("No Endpoint")),
+        None => return Err(ProfileError::NoEndpoint),
     };
     let count = ACROSS_RUNTIME
         .lock()
